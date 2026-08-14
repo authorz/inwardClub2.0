08e2e42775a4bd2ded0b9647fe65f80f81983d0e
authorz <1055271054@qq.com>
2026-08-14 20:06:26 +0800

    fix(miniprogram): restore stacked ticket scrolling

--- mini-program/miniprogram/pages/tickets/tickets.js
+++ mini-program/miniprogram/pages/tickets/tickets.js
@@ -34,7 +34,6 @@ Page({
     bucket: 'pending',
     all: [],
     list: [],
-    currentTicketIndex: 0,
     showCodeModal: false,
     codeTicket: null,
     qr: [],
@@ -72,16 +71,7 @@ Page({
   },
 
   applyFilter() {
-    this.setData({
-      list: this.data.all.filter((t) => t.bucket === this.data.bucket),
-      currentTicketIndex: 0,
-    });
-  },
-
-  onTicketChange(e) {
-    const current = Number(e.detail.current);
-    if (!Number.isInteger(current) || current === this.data.currentTicketIndex) return;
-    this.setData({ currentTicketIndex: current });
+    this.setData({ list: this.data.all.filter((t) => t.bucket === this.data.bucket) });
   },
 
   showCode(e) {
