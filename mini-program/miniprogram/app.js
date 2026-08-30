bb5830a0664dde9c6b8f6fdde186ee80aff4909e
authorz <1055271054@qq.com>
2026-08-30 18:58:42 +0800

    接入小程序版本更新重启提示

--- mini-program/miniprogram/app.js
+++ mini-program/miniprogram/app.js
@@ -9,6 +9,7 @@ App({
   },
 
   onLaunch(options) {
+    this.initUpdateManager();
     this.syncPendingInvitation(options);
     try {
       this.globalData.systemInfo = wx.getWindowInfo();
@@ -19,6 +20,29 @@ App({
       .ensure()
       .then(() => this.syncPendingInvitation(options))
       .catch(() => {});
+  },
+
+  initUpdateManager() {
+    if (typeof wx.getUpdateManager !== 'function') return;
+    const updateManager = wx.getUpdateManager();
+    updateManager.onUpdateReady(() => {
+      wx.showModal({
+        title: '版本更新',
+        content: '新版本已经准备好，需要重启小程序后生效。',
+        confirmText: '立即重启',
+        cancelText: '稍后',
+        success: (res) => {
+          if (res.confirm) updateManager.applyUpdate();
+        },
+      });
+    });
+    updateManager.onUpdateFailed(() => {
+      wx.showModal({
+        title: '更新失败',
+        content: '新版本下载失败，请退出后重新打开小程序重试。',
+        showCancel: false,
+      });
+    });
   },
 
   onShow(options) {
